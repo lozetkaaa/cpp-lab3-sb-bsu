fdbfa3856b2ea84437a86793be5629dd4efd979e
lozetkaaa <lozetka1@gmail.com>
2024-10-28 00:46:50 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -4,17 +4,24 @@ int main()
 {
     int k, i = 1, sum_k = 0, number;
     cout << " Enter the amount of integers to sum = "; cin >> k;
-    if (k == 0) {
-        // Do nothing if k is 0
+    while (cin.fail()) {
+        cin.clear();
+        cin.ignore();
+        cout << "Error! Enter the amount of INTEGERS to sum =  ";
+        cin >> k;
     }
-    else {
-        do {
-            cout << "Enter integer nr. " << i << ": ";
+    do {
+        if (k==0) { }
+        cout << "Enter integer nr. " << i << ": ";
+        cin >> number;
+        while (cin.fail()) {
+            cin.clear();
+            cin.ignore();
+            cout << "Error! Enter INTEGER nr.  ";
             cin >> number;
-            sum_k += number;
-            i++;
-        } while (i <= k);
-        cout << " The total sum of " << k << " integers is: " << sum_k;
-    }
+        }
+        sum_k += number; i++;
+    } while (i <= k);
+    cout << " The total sum of " << k << " integers is: " << sum_k;
     return 0;
 }
